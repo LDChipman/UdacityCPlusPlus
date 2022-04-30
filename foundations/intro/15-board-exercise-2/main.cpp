2456b3adc3471284c08aee51cff6d67669c078c1
Liam D Chipman <lchipman@localhost.localdomain>
2022-04-30 19:22:14 -0300

    moved the code from 15-board-exercise-2 to a seperate function now called from main and compiled it on linux. Further commits should all be from linux

--- foundations/intro/15-board-exercise-2/main.cpp
+++ foundations/intro/15-board-exercise-2/main.cpp
@@ -4,17 +4,23 @@
 using std::cout;
 using std::vector;
 
-int main() {
-  vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
-                               {0, 1, 0, 0, 0, 0},
-                               {0, 1, 0, 0, 0, 0},
-                               {0, 1, 0, 0, 0, 0},
-                               {0, 0, 0, 0, 1, 0}};
+void PrintBoard(vector<vector<int>> board) {
   for (vector<int> v : board) {
     for (int i : v) {
       cout << i << " ";
     }
     cout << "\n";
   }
+}
+
+int main() {
+  vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
+                               {0, 1, 0, 0, 0, 0},
+                               {0, 1, 0, 0, 0, 0},
+                               {0, 1, 0, 0, 0, 0},
+                               {0, 0, 0, 0, 1, 0}};
+
+  PrintBoard(board);
+
   return 0;
 }
