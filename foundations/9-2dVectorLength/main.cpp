06a8b3601412f75dba0aaf4136308d7e6a9505fb
Liam Chipman <chipman.liam@gmail.com>
2022-04-30 15:40:45 -0300

    added a program that prints the length of a vector ins a 2 dimensional vector

---
+++ foundations/9-2dVectorLength/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::vector;
+
+int main() {
+  vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
+                               {0, 1, 0, 0, 0, 0},
+                               {0, 1, 0, 0, 0, 0},
+                               {0, 1, 0, 0, 0, 0},
+                               {0, 0, 0, 0, 1, 0}};
+  cout << board[0].size() << "\n";
+  return 0;
+}
