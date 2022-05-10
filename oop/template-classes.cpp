362573f8d2cad29c8b6d5cceb3100c0638451ba0
Liam D Chipman <lchipman@localhost.localdomain>
2022-05-10 18:27:29 -0300

    added a generic class

---
+++ oop/template-classes.cpp
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <sstream>
+#include <string>
+
+template <typename KeyType, typename ValueType> class Mapping {
+public:
+  Mapping(KeyType key, ValueType value) : key(key), value(value) {}
+  std::string Print() const {
+    std::ostringstream stream;
+    stream << key << ": " << value;
+    return stream.str();
+  }
+  KeyType key;
+  ValueType value;
+};
+
+// Test
+int main() {
+  Mapping<std::string, int> mapping("age", 20);
+  assert(mapping.Print() == "age: 20");
+}
