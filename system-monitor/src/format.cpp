e422c3ad472bc28a1719c4b940ad57230d741198
Liam Chipman <chipman.liam@gmail.com>
2022-05-11 19:21:33 -0300

    fixed formatting issue in format.cpp that caused it to display a nine to the left of the number if the seconds digit was less than 10

--- system-monitor/src/format.cpp
+++ system-monitor/src/format.cpp
@@ -6,10 +6,14 @@ using std::string;
 
 string Format::ElapsedTime(long seconds) {
 
+  long hours = seconds / 3600;
+  seconds %= 3600;
   long minutes = seconds / 60;
-  seconds = seconds % 60;
-  long hours = minutes / 60;
-  minutes = minutes % 60;
+  seconds %= 60;
+
+  if (seconds < 10)
+    return std::to_string(hours) + ":" + std::to_string(minutes) + ":0" +
+           std::to_string(seconds);
 
   return std::to_string(hours) + ":" + std::to_string(minutes) + ":" +
          std::to_string(seconds);
