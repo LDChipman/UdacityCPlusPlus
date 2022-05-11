6c7ef3d7780f2ef991107fbbdf0c4a0c9582f24b
Liam Chipman <chipman.liam@gmail.com>
2022-05-11 19:47:32 -0300

    fixed the formatting issue again, time now displays with each having a minimum of two digits (eg. 02, 23, 59)

--- system-monitor/src/format.cpp
+++ system-monitor/src/format.cpp
@@ -6,15 +6,15 @@ using std::string;
 
 string Format::ElapsedTime(long seconds) {
 
-  long hours = seconds / 3600;
-  seconds %= 3600;
   long minutes = seconds / 60;
   seconds %= 60;
-
-  if (seconds < 10)
-    return std::to_string(hours) + ":" + std::to_string(minutes) + ":0" +
-           std::to_string(seconds);
-
-  return std::to_string(hours) + ":" + std::to_string(minutes) + ":" +
-         std::to_string(seconds);
+  long hours = minutes / 60;
+  minutes %= 60;
+  string twoDigitSeconds = std::to_string(seconds).insert(
+      0, 2 - std::to_string(seconds).length(), '0');
+  string twoDigitMinutes = std::to_string(minutes).insert(
+      0, 2 - std::to_string(minutes).length(), '0');
+  string minTwoDigitHours =
+      std::to_string(hours).insert(0, 2 - std::to_string(hours).length(), '0');
+  return minTwoDigitHours + ":" + twoDigitMinutes + ":" + twoDigitSeconds;
 }
