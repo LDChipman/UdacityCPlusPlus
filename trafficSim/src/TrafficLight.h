73df1873eda1a9e61705a8237801756887749ef0
Liam <chipman.liam@gmail.com>
2022-07-05 15:41:54 -0300

    Implemented TrafficLight Class per FP.1

--- trafficSim/src/TrafficLight.h
+++ trafficSim/src/TrafficLight.h
@@ -30,17 +30,22 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-class TrafficLight
+class TrafficLight : public TrafficObject
 {
 public:
-    // constructor / desctructor
+    enum TrafficLightPhase {red, green};
+    // constructor / destructor
 
     // getters / setters
 
     // typical behaviour methods
+    void waitForGreen();
+    void simulate();
+    TrafficLightPhase getCurrentPhase();
 
 private:
     // typical behaviour methods
+    void cycleThroughPhases();
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
@@ -48,6 +53,7 @@ private:
 
     std::condition_variable _condition;
     std::mutex _mutex;
+    TrafficLightPhase _currentPhase;
 };
 
 #endif
