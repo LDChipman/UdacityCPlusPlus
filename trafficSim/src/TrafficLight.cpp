30d11cc8d2908afaf2ce4f7f37b89faf331b7cc8
Liam <chipman.liam@gmail.com>
2022-07-06 16:30:52 -0300

    added functionality to the receive and waitforgreen methods

--- trafficSim/src/TrafficLight.cpp
+++ trafficSim/src/TrafficLight.cpp
@@ -4,16 +4,19 @@
 #include "TrafficLight.h"
 
 /* Implementation of class "MessageQueue" */
-
-/* 
+ 
 template <typename T>
 T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
+    std::unique_lock<std::mutex> lock(_mutex);
+    _condition.wait(lock);
+    T msg = _queue.begin();
+    _queue.pop_front();
+    return std::move(msg);
 }
-*/
 
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
@@ -29,7 +32,6 @@ void MessageQueue<T>::send(T &&msg)
 
 /* Implementation of class "TrafficLight" */
 
-/* 
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -40,13 +42,21 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        if (_messageQueue.receive() == green){
+            return;
+        }
+        
+    }
+    
 }
 
-TrafficLightPhase TrafficLight::getCurrentPhase()
+TrafficLight::TrafficLightPhase TrafficLight::getCurrentPhase()
 {
     return _currentPhase;
 }
-*/
 
 void TrafficLight::simulate(){
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
