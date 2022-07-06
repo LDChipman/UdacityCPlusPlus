d31fa44b34d9d2e4a3b0c39171551dab30aea061
Liam <chipman.liam@gmail.com>
2022-07-06 15:19:17 -0300

    created the cycleThroughPhases method with a dummy message queue and ran it when the trafficlights simulate method is called

--- trafficSim/src/TrafficLight.cpp
+++ trafficSim/src/TrafficLight.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <thread>
 #include "TrafficLight.h"
 
 /* Implementation of class "MessageQueue" */
@@ -40,19 +41,48 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 {
     return _currentPhase;
 }
+*/
 
-void TrafficLight::simulate()
-{
+void TrafficLight::simulate(){
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
+
 // virtual function which is executed in a thread
-void TrafficLight::cycleThroughPhases()
-{
+void TrafficLight::cycleThroughPhases(){
     // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
-}
+    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles.
+    std::chrono::time_point<std::chrono::system_clock> lastUpdate;
+    double cycleDuration = 1;
 
-*/
+    lastUpdate = std::chrono::system_clock::now();
+    while (true) {
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        long timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - lastUpdate).count();
+        if (timeSinceLastUpdate >= cycleDuration){
+
+            // Sleeps for 4 to 6 seconds.
+            std::this_thread::sleep_for(std::chrono::seconds((rand()%3)+4));
+
+            //Switches between green and red
+            switch (_currentPhase){
+            case red:
+                _currentPhase = green;
+                break;
+            
+            case green:
+                _currentPhase = red;
+                break;
+            }
+
+            // Sends the current phase to the queue
+            _messageQueue.send(std::move(_currentPhase));
+
+            lastUpdate = std::chrono::system_clock::now();
+        }
+    }
+}
