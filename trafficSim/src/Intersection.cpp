6a07369fa0a6a95c3483a62fb310df5d873830fa
Liam <chipman.liam@gmail.com>
2022-07-06 16:38:33 -0300

    added trafficlights to intersection and blocked vehicles until the light is green

--- trafficSim/src/Intersection.cpp
+++ trafficSim/src/Intersection.cpp
@@ -87,8 +87,17 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
     
     // FP.6b : use the methods TrafficLight::getCurrentPhase and TrafficLight::waitForGreen to block the execution until the traffic light turns green.
+    if (_trafficLight.getCurrentPhase() == TrafficLight::green)
+    {
+        lck.unlock();
+    } else
+    {
+        _trafficLight.waitForGreen();
+        lck.unlock();
+    }
+    
+    
 
-    lck.unlock();
 }
 
 void Intersection::vehicleHasLeft(std::shared_ptr<Vehicle> vehicle)
@@ -109,6 +118,7 @@ void Intersection::setIsBlocked(bool isBlocked)
 void Intersection::simulate() // using threads + promises/futures + exceptions
 {
     // FP.6a : In Intersection.h, add a private member _trafficLight of type TrafficLight. At this position, start the simulation of _trafficLight.
+    _trafficLight.simulate();
 
     // launch vehicle queue processing in a thread
     threads.emplace_back(std::thread(&Intersection::processVehicleQueue, this));
